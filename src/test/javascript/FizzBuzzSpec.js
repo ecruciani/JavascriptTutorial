9eced58417cfc7f2fad5f50f50043b83a082b74f
ecruciani <eric.cruciani.work@gmail.com>
2012-09-02 17:00:39 +0200

    use FizzBuzz as singleton

--- src/test/javascript/FizzBuzzSpec.js
+++ src/test/javascript/FizzBuzzSpec.js
@@ -2,7 +2,7 @@ describe("FizzBuzz Suite Test", function() {
 
     it("should get all multiples of 3 from a given collection", function() {
 
-        var fizzBuzz = new FizzBuzz();
+        var fizzBuzz = FizzBuzz();
         var givenCollection = [1, 3, 5, 15, 8, 6];
 
         var multiplesOf3 = fizzBuzz.getAllMultiplesOf3From(givenCollection);
@@ -15,7 +15,7 @@ describe("FizzBuzz Suite Test", function() {
 
     it("should get all multiples of 5 from a given collection", function() {
 
-        var fizzBuzz = new FizzBuzz();
+        var fizzBuzz = FizzBuzz();
         var givenCollection = [1, 3, 5, 15, 8, 6];
 
         var multiplesOf5 = fizzBuzz.getAllMultiplesOf5From(givenCollection);
@@ -27,7 +27,7 @@ describe("FizzBuzz Suite Test", function() {
 
     it("should get all multiple of 3 and 5 from a given collection", function() {
 
-        var fizzBuzz = new FizzBuzz();
+        var fizzBuzz = FizzBuzz();
         var givenCollection = [1, 3, 5, 15, 8, 6];
 
         var multiplesOf3And5 = fizzBuzz.getAllMultiplesOf3And5From(givenCollection);
@@ -41,7 +41,7 @@ describe("FizzBuzz Suite Test", function() {
 
     it("should print 'Fizz' for multiple of 3, 'Buzz' for multiple of 5, and 'FizzBuzz' for both multiple of 3 and 5", function () {
 
-        var fizzBuzz = new FizzBuzz();
+        var fizzBuzz = FizzBuzz();
         var givenCollection = new Array();
 
         for(var i = 1; i <= 100; i++){
